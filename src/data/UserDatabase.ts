a49b23a89bf3b6af46694a277502c8667c6789d6
Guilherme Lira <Guilhermealira@gmail.com>
2022-07-14 15:34:07 -0300

    Signup e Login OK

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -3,35 +3,25 @@ import { User } from "../model/User";
 
 export class UserDatabase extends BaseDatabase {
 
-  private static TABLE_NAME = "";
+  private static TABLE_NAME = "USUARIOS";
 
-  public async createUser(
-    id: string,
-    email: string,
-    name: string,
-    password: string,
-    role: string
-  ): Promise<void> {
+  public async createUser(id: string, email: string, name: string, password: string, role: string): Promise<void> {
     try {
-      await this.getConnection()
-        .insert({
-          id,
-          email,
-          name,
-          password,
-          role
-        })
-        .into(UserDatabase.TABLE_NAME);
-    } catch (error) {
-      throw new Error(error.sqlMessage || error.message);
-    }
+
+      await this.getConnection().insert({id, email, name, password, role}).into(UserDatabase.TABLE_NAME);
+
+    } catch (err) {
+      if (err instanceof Error) {
+          throw new Error(err.message)
+      } else {
+          throw new Error("Erro no createUser UserDatabase.")
+      }
+  }
   }
 
   public async getUserByEmail(email: string): Promise<User> {
-    const result = await this.getConnection()
-      .select("*")
-      .from(UserDatabase.TABLE_NAME)
-      .where({ email });
+
+    const result = await this.getConnection().select("*").from(UserDatabase.TABLE_NAME).where({ email });
 
     return User.toUserModel(result[0]);
   }
